bce6a19123ea1b1e11124864994528bd8f21168e
Cobaltboy <pathanaffan62@gmail.com>
2025-12-19 19:14:10 +0530

    teensy

--- frontend/duck-chat/vite.config.js
+++ frontend/duck-chat/vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/duck-chat/',
+  // base: '/duck-chat/',
   plugins: [react(), tailwindcss(),],
   resolve: {
     alias: {
